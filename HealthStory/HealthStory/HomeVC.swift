08f9f6ae47e31046a893696319ae911f1441d5b3
Santi Gracia <santigracia@santigracia.com>
2016-09-17 20:38:35 -0500

    setting up

---
+++ HealthStory/HealthStory/HomeVC.swift
@@ -0,0 +1,13 @@
+//
+//  HomeVC.swift
+//  HealthStory
+//
+//  Created by Santi on 9/17/16.
+//  Copyright © 2016 StoryTellers. All rights reserved.
+//
+
+import Foundation
+
+class HomeVC : UIViewController {
+
+}
---
+++ HealthStory/Pods/Target Support Files/Pods-HealthStory/Pods-HealthStory-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HealthStoryVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HealthStoryVersionString[];
+
